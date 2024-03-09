1d90656409ae039bbf8bcb8a7951cb493f8d6a29
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-03-09 10:49:20 -0600

    Libros Controller se agregó

--- PARCIAL1A/Controllers/LibrosController.cs
+++ PARCIAL1A/Controllers/LibrosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PARCIAL1A.Models;
 
 namespace PARCIAL1A.Controllers
@@ -27,6 +28,78 @@ namespace PARCIAL1A.Controllers
             {
                 return NotFound();
             }
+
+            return Ok(listaLibros);
+
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        public IActionResult Guardar([FromBody] Autores listaLibros)
+        {
+
+            try
+            {
+
+                _librosContext.Autores.Add(listaLibros);
+                _librosContext.SaveChanges();
+                return Ok(listaLibros);
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+
+            }
+
+        }
+
+        [HttpPut]
+        [Route("Put")]
+        public IActionResult Update(int id, [FromBody] Libros listaLibrosNueva)
+        {
+
+            Libros? listaLibrosActu = (from c in _librosContext.Libros
+                                         where c.Id == id
+                                         select c).FirstOrDefault();
+            if (listaLibrosActu == null)
+            {
+
+                return NotFound();
+
+            }
+
+            listaLibrosActu.Id = listaLibrosNueva.Id;
+            listaLibrosActu.Titulo = listaLibrosNueva.Titulo;
+
+
+            _librosContext.Entry(listaLibrosActu).State = EntityState.Modified;
+            _librosContext.SaveChanges();
+
+            return Ok(listaLibrosNueva);
+
+
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete(int id)
+        {
+
+            Libros? listaLibros = (from c in _librosContext.Libros
+                                     where c.Id == id
+                                     select c).FirstOrDefault();
+            if (listaLibros == null)
+            {
+
+                return NotFound();
+
+            }
+
+            _librosContext.Libros.Attach(listaLibros);
+            _librosContext.Libros.Remove(listaLibros);
+            _librosContext.SaveChanges();
 
             return Ok(listaLibros);
 
