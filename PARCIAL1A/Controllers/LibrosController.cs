16e248b92dfe13f49a4065e864acf93fc3faacf9
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-03-09 10:41:24 -0600

    Se solucionaron otros conflictos. Autores funcionando al 100.

---
+++ PARCIAL1A/Controllers/LibrosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PARCIAL1A.Models;
+
+namespace PARCIAL1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly librosContext _librosContext;
+
+        public LibrosController(librosContext librosContext)
+        {
+            _librosContext = librosContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll/")]
+        public IActionResult Get()
+        {
+
+            Libros? listaLibros = (from p in _librosContext.Libros
+                                     select p).FirstOrDefault();
+
+            if (listaLibros == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(listaLibros);
+
+        }
+
+    }
+}
