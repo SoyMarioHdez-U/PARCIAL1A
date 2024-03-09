8fb4af941085af6757e18802cb4a33487a6237c5
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-03-09 09:32:48 -0600

    Se cambiaron las mayúsculas

--- PARCIAL1A/Models/librosContext.cs
+++ PARCIAL1A/Models/librosContext.cs
@@ -11,7 +11,7 @@ namespace PARCIAL1A.Models
 
         public DbSet<libros> Libros { get; set; }
         public DbSet<Posts> Posts { get; set; }
-        public DbSet<autores> Autores { get; set; }
+        public DbSet<Autores> Autores { get; set; }
         public DbSet<AutorLibro> AutorLibros { get; set; }
     }
 }
