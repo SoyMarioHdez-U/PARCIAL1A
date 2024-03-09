62e86b239976e5c706ce62bde61064ac5b6c0d7b
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-03-09 08:53:23 -0600

    Se agregó la inyección del string de conexión

---
+++ PARCIAL1A/Models/librosContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace PARCIAL1A.Models
+{
+    public class librosContext : DbContext
+    {
+        public librosContext(DbContextOptions <librosContext> options): base(options) { 
+        
+        }
+    }
+}
--- PARCIAL1A/Program.cs
+++ PARCIAL1A/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using PARCIAL1A.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Inyección por dependencia del string de conexión al contexto
+builder.Services.AddDbContext<librosContext>(options =>
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("librosDbConnection"))
+);
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
