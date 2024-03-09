f0e26c771f13d9fa9b8e2d827d97a4d2bf47b72e
MAmonterrosa <rodrigo.monterrosa@catolica.edu.sv>
2024-03-09 09:33:06 -0600

    Guardados

--- PARCIAL1A/Controllers/postsController.cs
+++ PARCIAL1A/Controllers/postsController.cs
@@ -39,11 +39,11 @@ namespace PARCIAL1A.Controllers
 
         [HttpGet]
         [Route("GetId/{id}")]
-        public IActionResult Buscar(string id)
+        public IActionResult Buscar(int id)
         {
 
             Posts? listadeprueba = (from p in _librosContext.Posts
-                                         where p.id == id
+                                         where p.Id == id
                                          select p).FirstOrDefault();
 
             if (listadeprueba == null)
@@ -56,109 +56,7 @@ namespace PARCIAL1A.Controllers
         }
 
 
-        [HttpGet]
-        [Route("GetPruebaDeRelacion")]
-        public IActionResult GetDatosR()
-        {
 
-            List<Datos> listadeprueba = (from d in _pruebaContext.Datos
-
-                                         join su in _pruebaContext.Summero
-                                                 on d.id_adios equals su.id_adios
-
-                                         join s in _pruebaContext.Star
-                                                on d.id_que equals s.id_que
-
-                                         select d).ToList();
-
-            if (listadeprueba.Count() == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(listadeprueba);
-
-        }
-        [HttpGet]
-        [Route("GetTodo")]
-        public IActionResult GetTodo()
-        {
-
-           var listadeprueba = (from d in _pruebaContext.Datos
-
-                                         join su in _pruebaContext.Summero
-                                                 on d.id_adios equals su.id_adios
-
-                                         join s in _pruebaContext.Star
-                                                on d.id_que equals s.id_que
-
-                                         select new 
-                                         {
-                                         d.id_hola,
-                                         d.letra,
-                                         d.numero,
-                                         d.doble,
-                                         d.fecha,
-                                         adiositoID = d.id_adios, // Si un nombre de las variables es igual al nombre de una tabla se puede hacer esto 
-                                         su.tal,
-                                         su.byebye,
-                                         QUEEEEE = d.id_que,
-                                         s.Que,
-                                         s.Como,
-                                         s.Cuando,
-                                         s.Porque,
-                                         SumaDeIDs = $"Hola: {d.id_hola}, Adios: {su.id_adios}, Que: {s.id_que}" // Tambien se pueden unir varias variables
-                                         
-                                         }).ToList();
-
-            if (listadeprueba.Count() == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(listadeprueba);
-
-        }
-
-        [HttpGet]
-        [Route("GetConSalto")]
-        public IActionResult GetSaltos()
-        {
-
-            var listadeprueba = (from d in _pruebaContext.Datos
-
-                                 join su in _pruebaContext.Summero
-                                         on d.id_adios equals su.id_adios
-
-                                 join s in _pruebaContext.Star
-                                        on d.id_que equals s.id_que
-
-                                 select new
-                                 {
-                                     d.id_hola,
-                                     d.letra,
-                                     d.numero,
-                                     d.doble,
-                                     d.fecha,
-                                     adiositoID = d.id_adios, // Si un nombre de las variables es igual al nombre de una tabla se puede hacer esto 
-                                     su.tal,
-                                     su.byebye,
-                                     QUEEEEE = d.id_que,
-                                     s.Que,
-                                     s.Como,
-                                     s.Cuando,
-                                     s.Porque,
-                                     SumaDeIDs = $"Hola: {d.id_hola}, Adios: {su.id_adios}, Que: {s.id_que}" // Tambien se pueden unir varias variables
-
-                                 }).Take(1).ToList(); // take(1) nos dara el top, con skip(10) nos quitara 10 registros  y OrderBy(condicion => condicion.variable que se quiere ordenar), se le puede agregar más de una con ThenBy y la misma estructura
-
-            if (listadeprueba.Count() == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(listadeprueba);
-        }
 
 
 
@@ -190,7 +88,7 @@ namespace PARCIAL1A.Controllers
         public IActionResult Update(int id, [FromBody] Posts listadepruebaNueva)
         {
             Posts? listadeprueba = (from p in _librosContext.Posts // Al mismo modo, en la busqueda se le puede poner el ? para dar a entender que puede dar nulo y mostrara un resultado vacio
-                                     where p.id == id
+                                     where p.Id == id
                                      select p).FirstOrDefault();
             if (listadeprueba == null)
             {
@@ -199,11 +97,13 @@ namespace PARCIAL1A.Controllers
 
             }
 
-            listadeprueba.tal = listadepruebaNueva.tal;
-            listadeprueba.byebye = listadepruebaNueva.byebye;
+            listadeprueba.Titulo = listadepruebaNueva.Titulo;
+            listadeprueba.Contenido = listadepruebaNueva.Contenido;
+            listadeprueba.FechaPublicacion = listadepruebaNueva.FechaPublicacion;
+            listadeprueba.AutorId = listadepruebaNueva.AutorId;
 
-            _pruebaContext.Entry(listadeprueba).State = EntityState.Modified;
-            _pruebaContext.SaveChanges();
+            _librosContext.Entry(listadeprueba).State = EntityState.Modified;
+            _librosContext.SaveChanges();
 
             return Ok(listadepruebaNueva);
 
@@ -214,9 +114,9 @@ namespace PARCIAL1A.Controllers
         [Route("Delete")]
         public IActionResult Delete(int id)
         {
-            Summer? listadeprueba = (from s in _pruebaContext.Summero // Al mismo modo, en la busqueda se le puede poner el ? para dar a entender que puede dar nulo y mostrara un resultado vacio
-                                     where s.id_adios == id
-                                     select s).FirstOrDefault();
+            Posts? listadeprueba = (from p in _librosContext.Posts // Al mismo modo, en la busqueda se le puede poner el ? para dar a entender que puede dar nulo y mostrara un resultado vacio
+                                     where p.Id == id
+                                     select p).FirstOrDefault();
             if (listadeprueba == null)
             {
 
@@ -224,9 +124,9 @@ namespace PARCIAL1A.Controllers
 
             }
 
-            _pruebaContext.Summero.Attach(listadeprueba);
-            _pruebaContext.Summero.Remove(listadeprueba);
-            _pruebaContext.SaveChanges();
+            _librosContext.Posts.Attach(listadeprueba);
+            _librosContext.Posts.Remove(listadeprueba);
+            _librosContext.SaveChanges();
 
             return Ok(listadeprueba);
 
--- PARCIAL1A/Models/AutorLibro.cs
+++ PARCIAL1A/Models/AutorLibro.cs
@@ -1,12 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace PARCIAL1A.Models
 {
+    [Keyless]
     public class AutorLibro
     {
         
         public int AutorId { get; set; }
-        
         public int LibroId { get; set; }
         public int Orden { get; set; }
     }
